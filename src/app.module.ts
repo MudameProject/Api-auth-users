3c476c3a6f50334f6ebcebdb89ddb565731e1eb7
daniejl04 <danielestebanjimenezlopez@gmail.com>
2024-05-01 17:52:30 -0500

    add module persistance and .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { PersistanceModule } from './persistance/persistance.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [PersistanceModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
---
+++ src/persistance/persistance.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import dbConfig from './db.config';
+import { ConfigType } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
+@Module({
+  imports: [
+    MongooseModule.forRootAsync({
+      useFactory: (configService: ConfigType<typeof dbConfig>) => {
+        const { db, env } = configService;
+        const uriDb =
+          env === 'local'
+            ? `${db.connection}${db.host}/${db.name}`
+            : `mongodb+srv://danielestebanjimenezlopez:c6nZDMfbdQFEQ3gM@test.laxdtla.mongodb.net/`;
+        return {
+          uri: uriDb,
+        };
+      },
+      inject: [dbConfig.KEY],
+    }),
+  ],
+})
+export class PersistanceModule {}
